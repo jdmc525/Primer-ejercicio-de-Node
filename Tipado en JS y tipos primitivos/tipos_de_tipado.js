1dcf2f1acfffbf1b4a955bbf74b366ba68f3c60d
jdmc525 <121909687+jdmc525@users.noreply.github.com>
2023-01-03 19:08:19 -0600

    Add files via upload

---
+++ Tipado en JS y tipos primitivos/tipos_de_tipado.js
@@ -0,0 +1,27 @@
+//Number
+
+64612
+
+//STRINGS
+"Hola"
+
+//BOOLEANS
+true
+false
+
+//null & undefined
+
+null  //--->> Esta asignado en memoria pero esta vacio 
+undefined //----> No esta definido en memoria todavia 
+
+
+//Null, Undefined, False y 0 --->> Todas son falsas
+
+if (null) {
+    console.log("XD")
+} else [
+    console.log("XDn't")
+]
+
+//Resultado --> XDn't
+
