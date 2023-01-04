37760be2bb2f1348ddd9685bd8e19d4180d1db76
jdmc525 <121909687+jdmc525@users.noreply.github.com>
2023-01-04 17:58:28 -0600

    Add files via upload

---
+++ Listas, Objetos y Fechas en JS/Lista_tarea2.js
@@ -0,0 +1,12 @@
+
+
+var lista = [
+    "Daniel", 19, true, 26/04/2003,
+    libro= {
+        titulo: 1984,
+        autor: "George Orwell",
+        fecha: 1947,
+        url: "https://es.wikipedia.org/wiki/1984_(novela)"
+    } 
+]
+
