f1068ee57687ef8ea0a204eae00bfa45c1eea473
jdmc525 <121909687+jdmc525@users.noreply.github.com>
2023-01-04 18:04:22 -0600

    Update Lista_tarea2.js

--- Listas, Objetos y Fechas en JS/Lista_tarea2.js
+++ Listas, Objetos y Fechas en JS/Lista_tarea2.js
@@ -1,11 +1,9 @@
-
-
 var lista = [
-    "Daniel", 19, true, 26/04/2003,
+    "Daniel", 19, true, new Date(2003, 26, 5),
     libro= {
         titulo: 1984,
         autor: "George Orwell",
-        fecha: 1947,
+        fecha: new Date(1949, 8, 7),
         url: "https://es.wikipedia.org/wiki/1984_(novela)"
     } 
 ]
